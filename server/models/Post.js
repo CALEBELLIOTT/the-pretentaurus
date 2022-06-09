2040d38feff3e097a499835651b06b6dcc32c8aa
Conner Seely <Seely.Conner@gmail.com>
2022-06-09 15:40:42 -0600

    Post fix

--- server/models/Post.js
+++ server/models/Post.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 export const PostSchema = new Schema({
     title: { type: String, required: true },
     originalDescription: { type: String, required: true },
-    editedDescription: { type: Number, required: true, min: 1950, max: new Date().getFullYear() + 2 },
+    editedDescription: { type: String, required: true },
     numberOfLikes: { type: Number, required: true, min: 1 },
     likedBy: { type: String },
     imgUrl: { type: String, default: "https://media.vanityfair.com/photos/5d56eac902bf930008778de7/3:2/w_1998,h_1332,c_limit/obi-wan-ewan-series.jpg" },
