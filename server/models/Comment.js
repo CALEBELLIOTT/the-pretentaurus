fedc3994d08c8fe4d0514b869518d1a8dcb0cbb3
Conner Seely <Seely.Conner@gmail.com>
2022-06-09 15:35:50 -0600

    Schemas with DbContext

---
+++ server/models/Comment.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+export const CommentSchema = new Schema({
+    originalDescription: { type: String, required: true },
+    editedDescription: { type: String, required: true },
+    numberOfLikes: { type: Number, required: true },
+    likedBy: { type: String },
+    creatorId: { type: Schema.Types.ObjectId, ref: 'Account' },
+    postId: { type: Schema.Types.ObjectId, ref: 'Post', required: true }
+}, { timestamps: true, toJSON: { virtuals: true } })
+
+
+CommentSchema.virtual('post', {
+    localField: 'postId',
+    ref: 'Post',
+    foreignField: '_id',
+    justOne: true
+})
