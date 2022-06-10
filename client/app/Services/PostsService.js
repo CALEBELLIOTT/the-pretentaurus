54f0426d9cc5cb5f8b21905b314534cde18a8cc0
Caleb Elliott <caleb123elliott@gmail.com>
2022-06-10 12:15:46 -0600

    big modal wokring

--- client/app/Services/PostsService.js
+++ client/app/Services/PostsService.js
@@ -1,18 +1,25 @@
 
-// 
-// let api = axios.create({
-//   baseURL: 'http://localhost:3000/api/posts',
-//   withCredentials: true
-// })
 
+import { ProxyState } from "../AppState.js";
+import { Post } from "../Models/Post.js";
 import { api } from "./AxiosService.js";
 
 class PostsService {
+
+  async getPosts() {
+    const res = await api.get('api/posts')
+    console.log(res.data);
+    ProxyState.posts = res.data.map(p => new Post(p))
+  }
   async createPost(data) {
     const res = await api.post('api/posts', data)
     console.log(res.data);
   }
 
+  async deletePost(id) {
+    const res = await api.delete('api/posts/' + id)
+    console.log(res.data);
+  }
 
 }
 
