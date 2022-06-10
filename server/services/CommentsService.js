3d76487c709b11800620800aa6ad23da3be81c4e
Conner Seely <Seely.Conner@gmail.com>
2022-06-10 12:21:17 -0600

    update.id

--- server/services/CommentsService.js
+++ server/services/CommentsService.js
@@ -4,25 +4,27 @@ import { BadRequest, Forbidden } from "../utils/Errors";
 
 
 class CommentsService {
-    
+
     async getAll(query = {}) {
         return await dbContext.Comments.find(query)
     }
+
     async getById(id) {
         const foundComment = await dbContext.Comments.findById(id)
-        if (!foundComment){
-            throw new BadRequest ("No comments with that Id")
+        if (!foundComment) {
+            throw new BadRequest("No comments with that Id")
         }
         return foundComment
     }
+
     async create(body) {
         return await dbContext.Comments.create(body)
     }
 
-   async edit(update) {
-        let original = await this.getById(update)
+    async edit(update) {
+        let original = await this.getById(update.id)
         if (original.creatorId.toString() !== update.creatorId) {
-            throw new Forbidden ("You did not create this comment")
+            throw new Forbidden("You did not create this comment")
         }
         original.originalDescription = update.originalDescription || original.originalDescription
         original.editedDescription = update.editedDescription || original.originalDescription
@@ -36,8 +38,8 @@ class CommentsService {
 
     async remove(commentId, userId) {
         const comment = await this.getById(commentId)
-        if (comment.creatorId.toString() !== userId){
-            throw new Forbidden ("you did not create this comment")
+        if (comment.creatorId.toString() !== userId) {
+            throw new Forbidden("you did not create this comment")
         }
         await comment.remove()
         return `deleted your comment`
