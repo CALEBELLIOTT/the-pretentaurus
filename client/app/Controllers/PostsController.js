ce0379a4d91188ea68d846b8627bbc7a325624d0
Seth <seth7293@gmail.com>
2022-06-10 10:44:18 -0600

    controller and html

--- client/app/Controllers/PostsController.js
+++ client/app/Controllers/PostsController.js
@@ -1,10 +1,32 @@
+import { ProxyState } from "../AppState.js";
+import { postsService } from "../Services/PostsService.js";
 
+function _draw() {
+    let posts = ProxyState.posts
+    template = ''
+
+
+}
 
 
 export class PostController {
     constructor() {
         console.log('post controllers');
 
+    }
+
+    async createPost() {
+        window.event.preventDefault()
+        let form = window.event.target
+        let data = {
+            url: form.url.value,
+            title: form.title.value,
+            description: form.description.value
+
+        }
+
+        console.log(data);
+        await postsService.createPost()
     }
 
 }
