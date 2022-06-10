7f42a4c631cb0e577e5fee28b6986bdeb80b2ebd
Conner Seely <Seely.Conner@gmail.com>
2022-06-10 10:16:41 -0600

    Number of likes

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 export const CommentSchema = new Schema({
     originalDescription: { type: String, required: true },
     editedDescription: { type: String, required: true },
-    numberOfLikes: { type: Number, required: true },
+    numberOfLikes: { type: Number, min: 0, default: 0 },
     likedBy: { type: String },
     creatorId: { type: Schema.Types.ObjectId, ref: 'Account' },
     postId: { type: Schema.Types.ObjectId, ref: 'Post', required: true }
